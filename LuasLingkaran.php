df1087133a1ff0e6f55519ece238f4b4c0cdf7a3
Rizqyaa <rizqyasanria1407@gmail.com>
2025-11-04 22:21:49 +0700

    pertemuan3

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,24 +1,15 @@
 <?php
 
-class LuasLingkaran {
-    
+class LuasLingkaran{
+
     public const phi = 3.14;
     public int $jari;
-
-    public function tampil($nama = "roda") {
-        $rumus = new LuasLingkaran()::phi * ($this->jari * $this->jari);
-        echo "Lingkaran {$nama} adalah: ", $rumus;
-    }
-
-    public static function testing(){
-        echo"ini dari static";
-    }
+    
 }
 
-
 $lingkaran = new LuasLingkaran();
-$lingkaran->jari = 5;
-$lingkaran->tampil("ban");
-LuasLingkaran::testing();
+$lingkaran-> jari = 5;
 
-?>
+$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+
+echo "Luas Lingkaran: ", $rumus;
