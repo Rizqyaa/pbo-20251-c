b943d5a86df8989d945026df6cafcd759f7d02bd
Rizqyaa <rizqyasanria1407@gmail.com>
2025-11-08 18:38:21 +0700

    ini method

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,15 +1,25 @@
 <?php
 
-class LuasLingkaran{
-
+class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
-    
+
+    public function tampil($nama = "roda") {
+        $rumus = new LuasLingkaran()::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} adalah: ", $rumus;
+    }
+
+    public static function testing(){
+        echo"ini dari static";
+    }
 }
 
+
 $lingkaran = new LuasLingkaran();
-$lingkaran-> jari = 5;
+$lingkaran->jari = 5;
+$lingkaran->tampil("ban");
 
-$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+LuasLingkaran::testing();
+echo ".."
 
-echo "Luas Lingkaran: ", $rumus;
+?>
