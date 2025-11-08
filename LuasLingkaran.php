3290bc4fd7e606e8c92dbaf8ad6408dcc298d073
Rizqyaa <rizqyasanria1407@gmail.com>
2025-11-08 18:09:11 +0700

    first commit

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,28 +1,15 @@
+<?php
 
-<?php 
+class LuasLingkaran{
 
-namespace App\Math;
-
-class luasLingkaran {
-    
     public const phi = 3.14;
-    protected int $jari;
-
-    public function __construct($isijari = 1) {
-        $this->jari = $isijari;
-    }
-
-    public function tampil($nama) {
-    $rumus = luasLingkaran:: phi * ($this->jari * $this->jari);
-    echo "Luas {$nama} hasilnya adalah: {$rumus}";
-        
-    }
-    public static function testing() {
-        echo "<br>Ini adalah method static";
-    }
-
-    public function __destruct() {
-        echo "<br>ah cape ah";
-    }
+    public int $jari;
+    
 }
 
+$lingkaran = new LuasLingkaran();
+$lingkaran-> jari = 5;
+
+$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+
+echo "Luas Lingkaran: ", $rumus;
